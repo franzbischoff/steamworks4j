d59025a239adabe659c9c2e3105d0e577d6e46c1
Franz <franzbischoff@gmail.com>
2015-05-18 10:46:07 +0100

    .

--- build-natives/build-win.bat
+++ build-natives/build-win.bat
@@ -1,5 +1,5 @@
 PATH=C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\
-call "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" 
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" 
 premake5 --file=build-win.lua vs2013
 pause
 msbuild steamworks4j.sln /p:Configuration=ReleaseDLL /p:Platform=Win32 /t:Rebuild
